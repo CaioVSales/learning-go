b279b59e78453062ce55e8f90a044ed802feb6b1
Caio <caiovictorsalesdefreitas@gmail.com>
2022-06-29 20:45:51 -0300

    Tipagem

---
+++ learning/boolean.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Declaration of a boolean variable
+	n := true
+	fmt.Println(n)
+	// Testing boolean values
+	p := 1 == 2
+	m := 1 == 2
+	fmt.Printf("%v, %T", p, m)
+}
